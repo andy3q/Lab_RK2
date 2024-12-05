b3b1b41fcc2f28b1b6c6df9baad29218b223bfaa
Andy <kanatnuris0@gmail.com>
2024-12-06 00:43:12 +0500

    my lw

---
+++ src/main/java/com/nurislam/lab_rk2/LabRk2Application.java
@@ -0,0 +1,13 @@
+package com.nurislam.lab_rk2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabRk2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabRk2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nurislam/lab_rk2/LabRk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurislam.lab_rk2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabRk2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
